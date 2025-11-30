ff2c83de857d32e9eb61e3fc40a1d61cf4dbb3c7
Andrae Banwo <andra3b@gmail.com>
2025-11-30 10:12:15 +0000

    Buildsystem v9

--- libav/init.lua
+++ libav/init.lua
@@ -20,13 +20,26 @@ local defines = FFILoader.LoadDefinitions(
 
 defines.AVERROR_EAGAIN = -11
 
-local libav = {
-	avutil = FFILoader.CreateLibrary("avutil-"..tostring(defines.LIBAVUTIL_VERSION_MAJOR), defines, true),
-	avcodec = FFILoader.CreateLibrary("avcodec-"..tostring(defines.LIBAVCODEC_VERSION_MAJOR, defines), true),
-	avformat = FFILoader.CreateLibrary("avformat-"..tostring(defines.LIBAVFORMAT_VERSION_MAJOR), defines, true),
-	avdevice = FFILoader.CreateLibrary("avdevice-"..tostring(defines.LIBAVDEVICE_VERSION_MAJOR), defines, true),
-	avfilter = FFILoader.CreateLibrary("avfilter-"..tostring(defines.LIBAVFILTER_VERSION_MAJOR), defines, true),
-	swscale = FFILoader.CreateLibrary("swscale-"..tostring(defines.LIBSWSCALE_VERSION_MAJOR), defines, true)
-}
+local libav
+
+if jit.os == "Windows" then
+	libav = {
+		avutil = FFILoader.CreateLibrary("avutil-"..tostring(defines.LIBAVUTIL_VERSION_MAJOR), defines, true),
+		avcodec = FFILoader.CreateLibrary("avcodec-"..tostring(defines.LIBAVCODEC_VERSION_MAJOR, defines), true),
+		avformat = FFILoader.CreateLibrary("avformat-"..tostring(defines.LIBAVFORMAT_VERSION_MAJOR), defines, true),
+		avdevice = FFILoader.CreateLibrary("avdevice-"..tostring(defines.LIBAVDEVICE_VERSION_MAJOR), defines, true),
+		avfilter = FFILoader.CreateLibrary("avfilter-"..tostring(defines.LIBAVFILTER_VERSION_MAJOR), defines, true),
+		swscale = FFILoader.CreateLibrary("swscale-"..tostring(defines.LIBSWSCALE_VERSION_MAJOR), defines, true)
+	}
+else
+	libav = {
+		avutil = FFILoader.CreateLibrary("avutil.so."..tostring(defines.LIBAVUTIL_VERSION_MAJOR), defines, true),
+		avcodec = FFILoader.CreateLibrary("avcodec.so."..tostring(defines.LIBAVCODEC_VERSION_MAJOR, defines), true),
+		avformat = FFILoader.CreateLibrary("avformat.so."..tostring(defines.LIBAVFORMAT_VERSION_MAJOR), defines, true),
+		avdevice = FFILoader.CreateLibrary("avdevice.so."..tostring(defines.LIBAVDEVICE_VERSION_MAJOR), defines, true),
+		avfilter = FFILoader.CreateLibrary("avfilter.so."..tostring(defines.LIBAVFILTER_VERSION_MAJOR), defines, true),
+		swscale = FFILoader.CreateLibrary("swscale.so."..tostring(defines.LIBSWSCALE_VERSION_MAJOR), defines, true)
+	}
+end
 
 return libav
