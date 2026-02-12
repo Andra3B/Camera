a3d83b58122613ffdf83efd02927c232bf00ccff
Andrae <andra3b@gmail.com>
2026-02-12 18:55:13 +0000

    Multi object tracking

--- MotionTracker.lua
+++ MotionTracker.lua
@@ -1,128 +1,252 @@
 local MotionTracker = {}
 
-function MotionTracker.Create(width, height)
+function MotionTracker.Create(width, height)	
 	local self = Class.CreateInstance(Entity.Create(), MotionTracker)
 
-	self._Width = width
-	self._Height = height
-
-	self._MotionMask = love.graphics.newCanvas(width, height)
-	self._PreviousMotionMask = love.graphics.newCanvas(width, height)
+	self._BackgroundCanvas = love.graphics.newCanvas(width, height)
+	self._PreviousBackgroundCanvas = love.graphics.newCanvas(width, height)
 
 	self._ReductionCanvases = {}
 
-	repeat
-		local reductionCanvas = love.graphics.newCanvas(width, height, {format = "rgba32f", readable = true})
-		reductionCanvas:setWrap("clampzero")
-		reductionCanvas:setFilter("nearest")
+	self._MotionShapes = nil
+	self._LargestMotionShape = 0
 
-		table.insert(self._ReductionCanvases, reductionCanvas)
-
-		width = math.ceil(width * 0.5)
-		height = math.ceil(height * 0.5)
-	until width == 1 and height == 1
-
-	local reductionCanvas = love.graphics.newCanvas(1, 1, {format = "rgba32f", readable = true})
-	reductionCanvas:setWrap("clampzero")
-	reductionCanvas:setFilter("nearest")
-
-	table.insert(self._ReductionCanvases, reductionCanvas)
-
-	self._LowerThreshold = 0.01
-	self._HigherThreshold = 0.3
-
-	self._CenterOfMotion = nil
-	self._MotionCoverage = nil
+	self._MotionThreshold = 0.12
+	self._ShapeMinimumArea = 0.03
+	self._ShapeSearchRadius = 7
+	self._AdaptionRate = 0.1
+	
+	self._MaxSubdivisions = math.floor(math.log(math.min(width, height))/math.log(2))
+	self.Subdivisions = 6
 
 	return self
 end
 
-function MotionTracker:Update(currentFrame)
+function MotionTracker:Update(frame, immediateBackgroundUpdate)
 	love.graphics.push("all")
 	love.graphics.reset()
 
-	local motionMask = self._MotionMask
-	self._MotionMask = self._PreviousMotionMask
-	self._PreviousMotionMask = motionMask
-
-	love.graphics.setCanvas(self._MotionMask)
+	love.graphics.setCanvas(self._ReductionCanvases[1])
 	love.graphics.setShader(Shaders.MotionMask)
-	Shaders.MotionMask:send("PreviousMotionMask", self._PreviousMotionMask)
-	Shaders.MotionMask:send("LowerThreshold", self._LowerThreshold)
-	Shaders.MotionMask:send("HigherThreshold", self._HigherThreshold)
-	love.graphics.draw(currentFrame)
-	
-	love.graphics.setBlendMode("replace")
-
-	local encodeCanvas = self._ReductionCanvases[1]
-
-	love.graphics.setCanvas(encodeCanvas)
-	love.graphics.setShader(Shaders.COMEncode)
-	love.graphics.draw(self._MotionMask)
+	Shaders.MotionMask:send("Background", self._BackgroundCanvas)
+	Shaders.MotionMask:send("MotionThreshold", self._MotionThreshold)
+	love.graphics.draw(frame)
 
 	love.graphics.setShader(Shaders.Reduction)
 
 	for index = 2, #self._ReductionCanvases, 1 do
 		local previousReductionCanvas = self._ReductionCanvases[index - 1]
-
+		
 		love.graphics.setCanvas(self._ReductionCanvases[index])
-		Shaders.Reduction:send("TexelSize", {1/previousReductionCanvas:getPixelWidth(), 1/previousReductionCanvas:getPixelHeight()})
-		love.graphics.clear(1, 0, 0, 1)
+		Shaders.Reduction:send("TexelSize", {
+			1/previousReductionCanvas:getWidth(),
+			1/previousReductionCanvas:getHeight()
+		})
 		love.graphics.draw(previousReductionCanvas)
 	end
 
-	self._CenterOfMotion = nil
-	self._MotionCoverage = nil
+	love.graphics.setCanvas(self._BackgroundCanvas)
+	love.graphics.setShader(Shaders.BackgroundAdaption)
+	Shaders.BackgroundAdaption:send("PreviousBackground", self._PreviousBackgroundCanvas)
+	Shaders.BackgroundAdaption:send("AdaptionRate", immediateBackgroundUpdate and 1 or self._AdaptionRate)
+	love.graphics.draw(frame)
+
+	local backgroundCanvas = self._BackgroundCanvas
+	self._BackgroundCanvas = self._PreviousBackgroundCanvas
+	self._PreviousBackgroundCanvas = backgroundCanvas
+
+	self._MotionShapes = nil
 
 	love.graphics.pop()
 end
 
+function MotionTracker:GetWidth()
+	return self._BackgroundCanvas:getWidth()
+end
+
+function MotionTracker:GetHeight()
+	return self._BackgroundCanvas:getHeight()
+end
+
+function MotionTracker:GetBackground()
+	return self._BackgroundCanvas
+end
+
 function MotionTracker:GetMotionMask()
-	return self._ReductionCanvases[2]
+	return self._ReductionCanvases[#self._ReductionCanvases]
 end
 
-function MotionTracker:GetHigherThreshold()
-	return self._HigherThreshold
+function MotionTracker:GetMotionThreshold()
+	return self._MotionThreshold
 end
 
-function MotionTracker:SetHigherThreshold(threshold)
-	self._HigherThreshold = math.clamp(threshold, 0, 1)
+function MotionTracker:SetMotionThreshold(threshold)
+	self._MotionThreshold = math.clamp(threshold, 0, 1)
 end
 
-function MotionTracker:GetLowerThreshold()
-	return self._LowerThreshold
+function MotionTracker:GetShapeMinimumArea()
+	return self._ShapeMinimumArea
 end
 
-function MotionTracker:SetLowerThreshold(threshold)
-	self._LowerThreshold = math.clamp(threshold, 0, 1)
+function MotionTracker:SetShapeMinimumArea(area)
+	self._ShapeMinimumArea = math.clamp(area, 0, 1)
 end
 
-function MotionTracker:GetCenterOfMotion()
-	if not self._CenterOfMotion then
-		local COMData = self._ReductionCanvases[#self._ReductionCanvases]:newImageData()
-		local x, y, motionSum = COMData:getPixel(0, 0)
-		COMData:release()
+function MotionTracker:GetAdaptionRate()
+	return self._AdaptionRate
+end
 
-		self._CenterOfMotion = Vector2.Create(x, y)
-		self._MotionCoverage = motionSum / (self._Width * self._Height)
+function MotionTracker:GetShapeSearchRadius()
+	return self._ShapeSearchRadius
+end
+
+function MotionTracker:SetShapeSearchRadius(radius)
+	self._ShapeSearchRadius = math.max(radius, 1)
+end
+
+function MotionTracker:SetAdaptionRate(rate)
+	self._AdaptionRate = math.clamp(rate, 0, 1)
+end
+
+function MotionTracker:GetMotionShapes()
+	if not self._MotionShapes then
+		local blockGrid = self._ReductionCanvases[#self._ReductionCanvases]:newImageData()
+		local width, height = blockGrid:getDimensions()
+
+		local shapes = {}
+
+		local visited = {}
+		local toVisit = {}
+
+		local shape = nil
+		
+		for y = 0, height - 1, 1 do
+			for x = 0, width - 1, 1 do
+				local index = y*height + x
+				local motion = blockGrid:getPixel(x, y)
+
+				if not visited[index] and motion > 0.95 then
+					shape = {x, y, x + 1, y + 1}
+					table.insert(shapes, shape)
+
+					table.insert(toVisit, x)
+					table.insert(toVisit, y)
+
+					visited[index] = true
+				end
+
+				while #toVisit > 0 do
+					local x2 = table.remove(toVisit, 1)
+					local y2 = table.remove(toVisit, 1)
+
+					for subY = -self._ShapeSearchRadius, self._ShapeSearchRadius, 1 do
+						for subX = -self._ShapeSearchRadius, self._ShapeSearchRadius, 1 do
+							local x3, y3 = x2 + subX, y2 + subY
+							local subIndex = y3*height + x3
+
+							if
+								x3 >= 0 and x3 < width and y3 >= 0 and y3 < height and
+								not visited[subIndex]
+							then
+								local subMotion = blockGrid:getPixel(x3, y3)
+								
+								if subMotion > 0.95 then
+									shape[1] = math.min(shape[1], x3)
+									shape[2] = math.min(shape[2], y3)
+									shape[3] = math.max(shape[3], x3 + 1)
+									shape[4] = math.max(shape[4], y3 + 1)
+
+									table.insert(toVisit, x3)
+									table.insert(toVisit, y3)
+
+									visited[subIndex] = true
+								end
+							end
+						end
+					end
+				end
+			end
+		end
+
+		blockGrid:release()
+		
+		local largestArea = 0
+		local largestShape = 0
+
+		local index = 1
+		while index <= #shapes do
+			local x1, y1, x2, y2 = unpack(shapes[index])
+			
+			x1 = x1/width
+			y1 = y1/height
+			x2 = x2/width
+			y2 = y2/height
+
+			local area = (x2 - x1)*(y2 - y1)
+
+			if area >= self._ShapeMinimumArea then
+				if area > largestArea then
+					largestArea = area
+					largestShape = index
+				end
+
+				shapes[index] = {Vector2.Create(x1, y1), Vector2.Create(x2, y2)}
+
+				index = index + 1
+			else
+				table.remove(shapes, index)
+			end
+		end
+
+		self._MotionShapes = shapes
+		self._LargestMotionShape = largestShape
 	end
 	
-	return self._CenterOfMotion
+	return self._MotionShapes
 end
 
-function MotionTracker:GetMotionCoverage()
-	self:GetCenterOfMotion()
+function MotionTracker:GetLargestMotionShape()
+	self:GetMotionShapes()
 
-	return self._MotionCoverage
+	return self._LargestMotionShape
+end
+
+function MotionTracker:GetMaxSubdivisions()
+	return self._MaxSubdivisions
+end
+
+function MotionTracker:GetSubdivisions()
+	return self._Subdivisions
+end
+
+function MotionTracker:SetSubdivisions(subdivisions)
+	local width, height = self._BackgroundCanvas:getDimensions()
+	subdivisions = math.clamp(subdivisions, 0, self._MaxSubdivisions)
+
+	for _ = 1, #self._ReductionCanvases, 1 do
+		table.remove(self._ReductionCanvases):release()
+	end
+
+	for _ = 0, self._MaxSubdivisions - subdivisions, 1 do
+		local reductionCanvas = love.graphics.newCanvas(width, height, {format = "rgba8"})
+		reductionCanvas:setWrap("clampzero")
+		reductionCanvas:setFilter("nearest")
+		
+		table.insert(self._ReductionCanvases, reductionCanvas)
+
+		width, height = math.floor(width*0.5), math.floor(height*0.5)
+	end
+
+	self._Subdivisions = subdivisions
 end
 
 function MotionTracker:Destroy()
 	if not self._Destroyed then
-		self._MotionMask:release()
-		self._MotionMask = nil
+		self._BackgroundCanvas:release()
+		self._BackgroundCanvas = nil
 
-		self._PreviousMotionMask:release()
-		self._PreviousMotionMask = nil
+		self._PreviousBackgroundCanvas:release()
+		self._PreviousBackgroundCanvas = nil
 
 		for index = 1, #self._ReductionCanvases, 1 do
 			self._ReductionCanvases[index]:release()
@@ -130,7 +254,7 @@ function MotionTracker:Destroy()
 
 		self._ReductionCanvases = nil
 
-		self._CenterOfMotion = nil
+		self._MotionShapes = nil
 
 		Entity.Destroy(self)
 	end
